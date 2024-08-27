d466cec87c0273bf2216529d9d2163ceb504e3dc
Ashgatchat <Ashleighjg@gmail.com>
2024-08-27 08:13:41 -0400

    adding routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -27,7 +27,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
  try {
-   const productData = await Location.findByPk(req.params.id, {
+   const productData = await Product.findByPk(req.params.id, {
      include: [
        { model: Category },
        { model: Tag, through: 'ProductTag' }
@@ -124,8 +124,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+   try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No location found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 module.exports = router;
